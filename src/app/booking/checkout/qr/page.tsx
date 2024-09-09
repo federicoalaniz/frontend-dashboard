a86f6eca464a3dd9f1026fe0b6fa40b89d97d03a
federicoalaniz <fedealaniz@gmail.com>
2024-09-08 23:49:14 -0300

    se agrega codigo faltante

--- src/app/booking/checkout/qr/page.tsx
+++ src/app/booking/checkout/qr/page.tsx
@@ -109,13 +109,107 @@ function QRContent() {
             ?
             <>
               <div className="flex flex-col items-center justify-center h-full bg-gray-200 pb-10 pt-20">
-                <h1 className="w-[814px] text-left text-[36px] text-[#10004f]">
-                  <strong>Comprobante / eTicket</strong>
-                </h1>
-                <div className="w-[813px] h-auto bg-white my-6 rounded-lg shadow-lg">
-                  {/* contenido de la UI */}
+            {/* TODO DEBE IR DENTRO DE ESTE CONTAINER */}
+
+            <h1 className="w-[814px] text-left text-[36px] text-[#10004f]">
+              <strong>Comprobante / eTicket</strong>
+            </h1>
+            <div className="w-[813px] h-auto bg-white my-6 rounded-lg shadow-lg">
+              <div className="flex flex-row-w-full bg-[#3198A5] rounded-t-lg py-6 px-10">
+                <p className="font-semibold text-white text-[24px] text-justify">
+                  {MENSAJE_SALDO}
+                </p>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex flex-col w-1/3 m-[60px]">
+                  <div className="flex items-center bg-[#3198A5] h-[26px] w-[232px] mb-5">
+                    <Image src={minibus} alt="minibus" className="mx-2" />
+                    <p className="text-white font-semibold text-[14px]">
+                      EN MINIBUS
+                    </p>
+                  </div>
+                  <AVQRCode text={result.posId} />
+                  <div className="my-5">
+                    <p
+                      className={`${ruda.className} font-bold text-[#5C5C5C] text-[16px]`}
+                    >
+                      Código:
+                    </p>
+                    <p className="font-bold text-[24px]">
+                      {result.posId.substring(0, 6)}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col mt-[60px]">
+                  <div className="mb-6">
+                    <p
+                      className={`${ruda.className} font-bold text-[#5C5C5C] text-[16px]`}
+                    >
+                      Origen / Destino:
+                    </p>
+                    <p className="text-[28px] font-semibold text-[#10004f]">
+                      {`${formatAddress(departure.address)} / ${formatAddress(destiny.address)}`}
+                    </p>
+                  </div>
+                  <div className="mb-6">
+                    <p
+                      className={`${ruda.className} font-bold text-[#5C5C5C] text-[16px]`}
+                    >
+                      Salida:
+                    </p>
+                    <p className="text-[32px] font-semibold text-[#10004f]">
+                      {`${formatDate(new Date(departure.date))} / ${departure.time} `}
+                    </p>
+                  </div>
+                  <div className="mb-6">
+                    <p
+                      className={`${ruda.className} font-bold text-[#5C5C5C] text-[16px]`}
+                    >
+                      Desde:
+                    </p>
+                    <p className="text-[22px] font-semibold text-[#10004f]">
+                      {formatAddress(departure.address)}
+                    </p>
+                  </div>
+                  <div className="mb-6">
+                    <p
+                      className={`${ruda.className} font-bold text-[#5C5C5C] text-[16px]`}
+                    >
+                      Pasajero responsable de Viaje:
+                    </p>
+                    <p className="text-[32px] font-semibold text-[#10004f]">
+                      {responsable.firstName} {responsable.lastName}
+                    </p>
+                    <p className="text-[18px] font-semibold text-[#8B8B8B]">
+                      { passengers.adult > 1 ? `${passengers.adult - 1} acompañante(s) ` : null }
+                      { passengers.baby > 0 ? `${passengers.baby} bebé(s)` : null }
+                      { passengers.pets.small + passengers.pets.big > 0 ? `${passengers.pets.small + passengers.pets.big} mascota(s)` : null }
+                    </p>
+                  </div>
                 </div>
               </div>
+              <div className="flex flex-row items-center justify-center mb-10 mt-6">
+                <button className="outline-none border-2 border-orange-500 rounded-md h-[40px] inline-flex items-center px-6 mx-5">
+                  Descargar
+                  <Image src={download} alt="download" className="ml-2" />
+                </button>
+                <button className="outline-none border-2 border-orange-500 rounded-md h-[40px] inline-flex items-center px-6">
+                  Compartir
+                  <Image src={share} alt="share" className="ml-2" />
+                </button>
+              </div>
+            </div>
+            {/* FIN DEL CONTAINER */}
+            <div className="font-bold text-orange-500 text-[18px]">
+                <button
+                  onClick={() => {
+                    localStorage.clear();
+                    redirect("/booking")
+                  }}
+                  className="bg-inherit shadow-none text-orange-500 focus:ring-transparent"
+                >Continuar en el sitio</button>
+            </div>
+          </div>
             </>
             :
             <>
